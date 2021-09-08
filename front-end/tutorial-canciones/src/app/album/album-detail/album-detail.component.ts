cf926d896444314bb49a04b1b9311696de398ae9
Juan Vanegas <j.vanegaso@uniandes.edu.co>
2021-09-08 18:32:28 -0500

    IC-69: Agregar servicio para enviar comentario (FE)

--- front-end/tutorial-canciones/src/app/album/album-detail/album-detail.component.ts
+++ front-end/tutorial-canciones/src/app/album/album-detail/album-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Album } from '../album';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-detail',
@@ -11,13 +13,15 @@ export class AlbumDetailComponent implements OnInit {
 
   @Input() album: Album;
   @Output() deleteAlbum = new EventEmitter();
-  
+
   userId: number;
   token: string;
 
   constructor(
     private routerPath: Router,
     private router: ActivatedRoute,
+    private albumService: AlbumService,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit() {
@@ -32,9 +36,23 @@ export class AlbumDetailComponent implements OnInit {
   goToJoinCancion(){
     this.routerPath.navigate([`/albumes/join/${this.album.id}/${this.userId}/${this.token}`])
   }
-  
+
   eliminarAlbum(){
     this.deleteAlbum.emit(this.album.id)
+  }
+
+  agregarComentario(){
+    if (!this.album?.id) {
+      return;
+    }
+    this.albumService.agregarComentario(this.album.id, 'Comentario de Prueba', this.token)
+      .subscribe(respuesta => {
+        if (respuesta && respuesta.data) {
+          this.toastr.success('El comentario fue agregado satisfactoriamente', "Comentario agregado");
+          return;
+        }
+        this.toastr.error('Ocurrió un error agregando el comentario. Intenta de nuevo, por favor.', 'Error al comentar');
+      });
   }
 
 }
