2598f5bd01da0e9bdc3cbea0dcd3411bacd1795d
Andrés Cendales <andres01660@gmail.com>
2021-09-08 23:01:46 -0500

    se incluye valor default en uri de base de datos

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("db_uri")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("db_uri", "sqlite:///ionic.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
