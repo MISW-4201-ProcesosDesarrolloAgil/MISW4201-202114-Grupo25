dae508d8f42789d8dee1f5fb5f02d975683a4b64
Juan Vanegas <j.vanegaso@uniandes.edu.co>
2021-09-10 19:28:24 -0500

    IC-57: Agregar interfaz para compartir canción

--- front-end/tutorial-canciones/src/app/usuario/usuario.service.ts
+++ front-end/tutorial-canciones/src/app/usuario/usuario.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
   })
 export class UsuarioService {
 
-    private backUrl: string = "http://localhost:5000"
+    private backUrl: string = environment.baseUrl;
 
     constructor(private http: HttpClient) { }
 
